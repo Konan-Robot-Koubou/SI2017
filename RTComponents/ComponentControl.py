54ac30b697fdf9c71f7686ef64de0a5eef197ba1
robot <konanrobotkoubou@gmail.com>
2017-11-27 11:54:54 +0900

    RTComponents

---
+++ RTComponents/ComponentControl.py
@@ -0,0 +1,81 @@
+# coding:utf-8
+
+import RPi.GPIO as GPIO
+import pi2go
+import os
+from time import sleep
+import subprocess
+
+sleep(10)
+
+class ActiveComp:
+    def startComp(self):
+
+        #Run Component
+        os.system("sudo python /home/pi/Desktop/RTC/pi2goRTC.py &")
+        os.system("sudo python /home/pi/Desktop/RTC/IRSensorRTC.py &")
+
+        #Wait time
+        sleep(0.8)
+
+    def rtshell(self):
+        #RTShell Command
+        os.system("rtcon /localhost/raspberrypi.host_cxt/pi2goRTC0.rtc:SpeedIn /localhost/raspberrypi.host_cxt/IRSensor0.rtc:SpeedOut")#Connect Component
+        os.system("rtcon /localhost/raspberrypi.host_cxt/pi2goRTC0.rtc:IRSensor /localhost/raspberrypi.host_cxt/IRSensor0.rtc:IRSensorValue")#Connect Component
+        print "Setting Complete"
+
+        #Wait Time
+        sleep(0.5)
+
+    def ActiveComp(self):
+        #Active component
+        os.system("rtact /localhost/raspberrypi.host_cxt/IRSensor0.rtc /localhost/raspberrypi.host_cxt/pi2goRTC0.rtc")
+
+
+class DeactiveComp:
+    def Deactive_Comp(self):
+        #Deact Component
+        print "Deactive start"
+        os.system("rtdeact /localhost/raspberrypi.host_cxt/pi2goRTC0.rtc /localhost/raspberrypi.host_cxt/IRSensor0.rtc")
+
+        #Wait time
+        sleep(0.5)
+
+    def exit_Comp(self):
+        #Component Exit
+        os.system("rtexit /localhost/raspberrypi.host_cxt/pi2goRTC0.rtc")
+        os.system("rtexit /localhost/raspberrypi.host_cxt/IRSensor0.rtc")
+        sleep(3)
+        print "All Components exit"
+
+   
+class check:
+    def ActivateCheck(self):
+        #Check status is Active
+	#Raspberry Piの中で動いているプロセスから、同じコンポーネントが動いていないかを確認する
+	#同じ名前のプロセスがなければret = 1、もしあればret = 0を返す
+	ret = subprocess.call("ps -ax | grep pi2goRTC.py | grep -v grep",shell=True)
+	print ret
+	return ret == 1
+
+os.system("echo y | rtm-naming")
+
+check = check()
+pi2go.init()
+
+try:
+	while True:
+		if pi2go.getSwitch() == True:
+                   if check.ActivateCheck() == True:
+                       state = ActiveComp()   
+                       state.startComp()
+                       state.rtshell()
+                       state.ActiveComp()
+
+                   else:
+                       state = DeactiveComp()
+                       state.Deactive_Comp()
+                       state.exit_Comp()
+                    
+except KeyboardInterrupt:
+    pass
